a71706bd1c86b4594c0293f64a7cf86f7aba7503
mhmdfjr <moh.fajar1304@gmail.com>
2026-08-27 17:29:30 +0700

    Fix: trust proxy Railway supaya asset URL generate sebagai https://

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,14 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // untuk Railway
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR |
+                     Request::HEADER_X_FORWARDED_HOST |
+                     Request::HEADER_X_FORWARDED_PORT |
+                     Request::HEADER_X_FORWARDED_PROTO
+        );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
